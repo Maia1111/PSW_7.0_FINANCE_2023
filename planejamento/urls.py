9259fc085e590313c6cf5f126499028dd693d4d0
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-07-08 14:44:57 -0400

    Concluido o envio de dados de planejamento para backend

--- planejamento/urls.py
+++ planejamento/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('definir_planejamento/', views.definir_planejamento, name='definir_planejamento'),
-
+    path('update_valor_planejamento_categoria/<int:id>', views.update_valor_planejamento_categoria, name="update_valor_planejamento_categoria"),
 ]
--- planejamento/views.py
+++ planejamento/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from perfil.models import Categoria
+import json
+from django.contrib.messages import constants
+from django.contrib import messages
+
+
 
 
 
@@ -8,3 +13,13 @@ def definir_planejamento(request):
     categorias = Categoria.objects.all()
     return render(request, 'definir_planejamento.html', {'categorias': categorias})
 
+
+def update_valor_planejamento_categoria(request, id):      
+    novo_valor = json.load(request)['novo_valor']
+    categoria = Categoria.objects.filter(id=id)
+    categoria.update(valor_planejamento=novo_valor)
+    return JsonResponse({'status':'sucesso'})
+
+
+    
+   
